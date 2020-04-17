e0d824b71e8edf6adcd4b5024f78254d41f72183
Runqi Luo <richie.runqi@gmail.com>
2020-04-17 19:33:30 -0400

    add one template, two models(one migrated, one not yet), created superuser, everything works

--- Insta/models.py
+++ Insta/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from imagekit.models import ProcessedImageField
 # Create your models here.
+
+class Post(models.Model):
+    title = models.TextField(blank=True, null=True) #no title or blank is fine
+    image = ProcessedImageField(
+        upload_to='static/image/posts',
+        format='JPEG',
+        options={'quality':100},
+        blank=True,
+        null=True
+    )
+
+class PostTwo(models.Model):
+    title = models.TextField(blank=True, null=True) #no title or blank is fine
+    image = ProcessedImageField(
+        upload_to='static/image/posts',
+        format='JPEG',
+        options={'quality':100},
+        blank=True,
+        null=True
+    )
