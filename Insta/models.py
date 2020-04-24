dfcfe777244d9da637e3af3fbdbe42b5b16e3e00
Runqi Luo <richie.runqi@gmail.com>
2020-04-24 11:24:43 -0400

    like feature added, show profile page, show only following posts, global header

--- Insta/models.py
+++ Insta/models.py
@@ -5,7 +5,38 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
+#After creating this model, should remember to migrate, otherwise database is wrong
+class InstaUser(AbstractUser):
+    profile_pic = ProcessedImageField(
+        upload_to='static/image/profiles',
+        format='JPEG',
+        options={'quality':100},
+        blank=True,
+        null=True
+    )
+    def get_connections(self):
+        #.get can only get one, so we use filter
+        connections = UserConnection.objects.filter(creator=self)
+        return connections
+
+    def get_followers(self):
+        followers = UserConnection.objects.filter(following=self)
+        return followers
+
+    #check if self is followed by this user
+    def is_followed_by(self, user):
+        followers = UserConnection.objects.filter(following=self)
+        return followers.filter(creator=user).exists()
+
+
+
 class Post(models.Model):
+    author = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='my_posts'
+    )
     title = models.TextField(blank=True, null=True) #no title or blank is fine
     image = ProcessedImageField(
         upload_to='static/image/posts',
@@ -18,12 +49,49 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse("post_detail", args=[str(self.id)])
 
-#After creating this model, should remember to migrate, otherwise database is wrong
-class InstaUser(AbstractUser):
-    profile_pic = ProcessedImageField(
-        upload_to='static/image/profiles',
-        format='JPEG',
-        options={'quality':100},
-        blank=True,
-        null=True
+    def get_like_count(self):
+        return self.likes.count()
+
+
+#record A(creator) follows B
+class UserConnection(models.Model):
+    #editable false means this created is immutable field
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    creator = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name="friendship_creator_set")
+    following = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name="friend_set")
+
+    def __str__(self):
+        return self.creator.username + ' follows ' + self.following.username
+
+
+#link model, link instauser and post
+class Like(models.Model):
+    #this post, is a foreign key point to Post model
+    post = models.ForeignKey(
+        Post,
+        # if one post is deleted, likes on this post get deleted too
+        on_delete=models.CASCADE,
+
+        # we can use post1.likes to get list of like relationship(A like post1, B like post1)
+        related_name='likes'
     )
+
+    user = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        #A.likes can find all likes clicked by A, also list of like relationship, but only A like xxx
+        related_name='likes'
+    )
+
+    class Meta:
+        # A like post1 cannot be duplicate, this combination(user, post) is unqiue
+        unique_together = ("post", "user");
+
+    def __str__(self):
+        return 'Like: ' + self.user.username + ' likes ' + self.post.title
--- Insta/views.py
+++ Insta/views.py
@@ -1,6 +1,8 @@
 from django.views.generic import TemplateView, ListView, DetailView
 
-from Insta.models import Post
+from annoying.decorators import ajax_request
+
+from Insta.models import Post, Like, InstaUser, UserConnection
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
@@ -20,14 +22,26 @@ class HelloWorld(TemplateView):
     template_name = 'test.html'
 
 
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class UserDetailView(DetailView):
+    model = InstaUser
+    template_name = 'user_detail.html'
+
 #this will return self.object_list(models of post) to template(index.html)
 class PostsView(ListView):
     model = Post
     template_name = 'index.html'
+    #override default get_queryset so that not all posts are returned
+    def get_queryset(self):
+        current_user = self.request.user
+        following = set()
+        for conn in UserConnection.objects.filter(creator=current_user).select_related('following'):
+            following.add(conn.following)
+        return Post.objects.filter(author__in=following)
 
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'post_detail.html'
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
@@ -50,4 +64,29 @@ class SignUp(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'signup.html'
     success_url = reverse_lazy("login")
+
+#different from above, not class based view
+#this means this function exist to respond to ajax request, that's why it don't need to render to a template
+@ajax_request
+def addLike(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        #save to database, will have exception if this user already liked the post (bcz we set unique-together in models.py)
+        like.save()
+        result = 1
+
+    except Exception as e:
+        #handle exception, which means remove like
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        #result 0 or 1 tells if it's remove like or add like
+        result = 0
+
+    #ajax return type is json, return to index.js
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
 
